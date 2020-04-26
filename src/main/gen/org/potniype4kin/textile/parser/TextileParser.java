9f39245344ff193f71defd819ba4febbddffd066
Denis Babochenko <babochenko.denis@yandex.ru>
2020-04-26 20:26:21 +0300

    [bnf] generate BNF definitions

---
+++ src/main/gen/org/potniype4kin/textile/parser/TextileParser.java
@@ -0,0 +1,85 @@
+// This is a generated file. Not intended for manual editing.
+package org.potniype4kin.textile.parser;
+
+import com.intellij.lang.PsiBuilder;
+import com.intellij.lang.PsiBuilder.Marker;
+import static org.potniype4kin.textile.psi.TextileType.*;
+import static com.intellij.lang.parser.GeneratedParserUtilBase.*;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.tree.TokenSet;
+import com.intellij.lang.PsiParser;
+import com.intellij.lang.LightPsiParser;
+
+@SuppressWarnings({"SimplifiableIfStatement", "UnusedAssignment"})
+public class TextileParser implements PsiParser, LightPsiParser {
+
+  public ASTNode parse(IElementType t, PsiBuilder b) {
+    parseLight(t, b);
+    return b.getTreeBuilt();
+  }
+
+  public void parseLight(IElementType t, PsiBuilder b) {
+    boolean r;
+    b = adapt_builder_(t, b, this, null);
+    Marker m = enter_section_(b, 0, _COLLAPSE_, null);
+    r = parse_root_(t, b);
+    exit_section_(b, 0, m, t, r, true, TRUE_CONDITION);
+  }
+
+  protected boolean parse_root_(IElementType t, PsiBuilder b) {
+    return parse_root_(t, b, 0);
+  }
+
+  static boolean parse_root_(IElementType t, PsiBuilder b, int l) {
+    return simpleFile(b, l + 1);
+  }
+
+  /* ********************************************************** */
+  // HEADER_START HEADER_TEXT
+  static boolean header(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "header")) return false;
+    if (!nextTokenIs(b, HEADER_START)) return false;
+    boolean r;
+    Marker m = enter_section_(b);
+    r = consumeTokens(b, 0, HEADER_START, HEADER_TEXT);
+    exit_section_(b, m, null, r);
+    return r;
+  }
+
+  /* ********************************************************** */
+  // header|list|TEXT
+  static boolean item(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "item")) return false;
+    boolean r;
+    r = header(b, l + 1);
+    if (!r) r = list(b, l + 1);
+    if (!r) r = consumeToken(b, TEXT);
+    return r;
+  }
+
+  /* ********************************************************** */
+  // LIST_DELIM LIST_TEXT
+  static boolean list(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "list")) return false;
+    if (!nextTokenIs(b, LIST_DELIM)) return false;
+    boolean r;
+    Marker m = enter_section_(b);
+    r = consumeTokens(b, 0, LIST_DELIM, LIST_TEXT);
+    exit_section_(b, m, null, r);
+    return r;
+  }
+
+  /* ********************************************************** */
+  // item*
+  static boolean simpleFile(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "simpleFile")) return false;
+    while (true) {
+      int c = current_position_(b);
+      if (!item(b, l + 1)) break;
+      if (!empty_element_parsed_guard_(b, "simpleFile", c)) break;
+    }
+    return true;
+  }
+
+}
---
+++ src/main/gen/org/potniype4kin/textile/psi/TextileType.java
@@ -0,0 +1,19 @@
+// This is a generated file. Not intended for manual editing.
+package org.potniype4kin.textile.psi;
+
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.PsiElement;
+import com.intellij.lang.ASTNode;
+
+public interface TextileType {
+
+
+  IElementType HEADER_START = new TextileToken("HEADER_START");
+  IElementType HEADER_TEXT = new TextileToken("HEADER_TEXT");
+  IElementType LIST_DELIM = new TextileToken("LIST_DELIM");
+  IElementType LIST_TEXT = new TextileToken("LIST_TEXT");
+  IElementType TEXT = new TextileToken("TEXT");
+
+  class Factory {
+  }
+}
