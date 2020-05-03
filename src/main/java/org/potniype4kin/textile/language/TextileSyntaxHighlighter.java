ff486957f8bffd999ad9a2161a085b994d083271
Denis Babochenko <babochenko.denis@yandex.ru>
2020-05-03 17:44:15 +0300

    [lex] highlight {code} language name

--- src/main/java/org/potniype4kin/textile/language/TextileSyntaxHighlighter.java
+++ src/main/java/org/potniype4kin/textile/language/TextileSyntaxHighlighter.java
@@ -29,6 +29,7 @@ public class TextileSyntaxHighlighter extends SyntaxHighlighterBase {
         textKeysByElementType.put(TextileType.HEADER_TEXT, () -> pack(headerText()));
         textKeysByElementType.put(TextileType.LIST_DELIM, () -> pack(listDelim()));
         textKeysByElementType.put(TextileType.CODE_DEF, () -> pack(code()));
+        textKeysByElementType.put(TextileType.CODE_LANGUAGE, () -> pack(code()));
         textKeysByElementType.put(TextileType.CODE_DEF_END, () -> pack(code()));
         textKeysByElementType.put(TextileType.CODE_DELIM, () -> pack(code()));
         textKeysByElementType.put(TextileType.CODE, () -> pack(code()));
